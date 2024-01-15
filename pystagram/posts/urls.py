dea7a2404ae87faaf0a8935dff5f5455baa9ffa7
lsy3709 <lsy3709@naver.com>
2024-01-15 17:00:50 +0900

    pystargram 동적 url 작업

--- pystagram/posts/urls.py
+++ pystagram/posts/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 # 추가
 from posts.views import feeds, comment_add, comment_delete, post_add
 
+# 추가
+app_name = 'posts'
 urlpatterns = [
-    path('feeds/', feeds),
+    path('feeds/', feeds, name="feeds"),
     #추가
-    path('comment_add/', comment_add),
+    path('comment_add/', comment_add, name="comment_add"),
 #추가
-    path('comment_delete/<int:comment_id>/', comment_delete),
+    path('comment_delete/<int:comment_id>/', comment_delete, name="comment_delete"),
 #추가
-    path('post_add/',post_add),
+    path('post_add/',post_add, name="post_add"),
 ]
--- pystagram/users/urls.py
+++ pystagram/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from users.views import login_view, logout_view, signup
 
+#추가
+app_name = 'users'
 urlpatterns = [
-    path('login/', login_view),
-    path('logout/', logout_view),
-    path('signup/', signup),
+    path('login2/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup, name='signup'),
 ]
