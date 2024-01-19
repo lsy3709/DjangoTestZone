544f4dea94119a6d24a9cb7b309ddc93abaac85f
lsy3709 <lsy3709@naver.com>
2024-01-19 11:43:57 +0900

    pystargram 회원가입시 이메일 추가 받고, 마이페이지,이메일, 소개글, 마지막 접속일 추가 작업 완료

--- pystagram/users/forms.py
+++ pystagram/users/forms.py
@@ -20,6 +20,7 @@ class LoginForm(forms.Form):
 
 class SignupForm(forms.Form):
     username = forms.CharField()
+    email = forms.EmailField()
     password1 = forms.CharField(widget=forms.PasswordInput())
     password2 = forms.CharField(widget=forms.PasswordInput())
     profile_image = forms.ImageField()
@@ -41,12 +42,14 @@ class SignupForm(forms.Form):
 
     def save(self):
         username = self.cleaned_data["username"]
+        email = self.cleaned_data["email"]
         password1 = self.cleaned_data["password1"]
         profile_image = self.cleaned_data["profile_image"]
         short_description = self.cleaned_data["short_description"]
 
         user = User.objects.create_user(
             username=username,
+            email=email,
             password=password1,
             profile_image=profile_image,
             short_description=short_description,
