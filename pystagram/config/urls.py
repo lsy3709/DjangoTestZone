efd53c0602a44c48f591ef28814e8ece367d4d9a
lsy3709 <lsy3709@naver.com>
2024-01-25 17:43:57 +0900

    비밀번호 초기화 메일 보내기 완료

--- pystagram/config/urls.py
+++ pystagram/config/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from config.views import index
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # 추가
@@ -29,6 +29,11 @@ urlpatterns = [
     # 추가
     path('posts/', include("posts.urls")),
     path('', index),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 urlpatterns += static(
--- pystagram/users/urls.py
+++ pystagram/users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import PasswordResetView
 from django.urls import path
 
-from users.forms import CustomPasswordResetForm
+
 from users.views import login_view, logout_view, signup, profile, followers, following, follow, profile_edit, user_edit, \
     update_password, delete_user, reset_password,send_email
 
@@ -15,7 +15,6 @@ urlpatterns = [
     path('send_email/', send_email, name='send_email'),
     path('<int:user_id>/update_password/', update_password, name='update_password'),
     # path('<int:user_id>/reset_password/', reset_password, name='reset_password'),
-    path('password_reset/', PasswordResetView.as_view(form_class=CustomPasswordResetForm), name='password_reset'),
     path('<int:user_id>/delete_user/', delete_user, name='delete_user'),
     path('<int:user_id>/profile/', profile, name='profile'),
     path('<int:user_id>/profile/edit/', profile_edit, name='profile_edit'),
