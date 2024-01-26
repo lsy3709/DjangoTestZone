f183c4d79ed9fdd05d5bfd8db902cb438a58613b
lsy3709 <lsy3709@naver.com>
2024-01-26 14:27:03 +0900

    이메일 보내기 함수 리팩토링 설정

--- pystagram/users/views.py
+++ pystagram/users/views.py
@@ -1,19 +1,20 @@
-#추가
+# 추가
 import random
 
+from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
 from django.core.mail import EmailMessage
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-#추가
+# 추가
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
-#추가
+# 추가
 from django.shortcuts import render, redirect, get_object_or_404
 
 from posts.models import Post
-#추가
+# 추가
 from users.forms import LoginForm, SignupForm, CustomUserChangeForm, CustomPasswordResetForm
 from users.models import User
 
@@ -25,19 +26,19 @@ def login_view(request):
         # 수정
         return redirect("posts:feeds")
 
-    #수정
+    # 수정
     if request.method == "POST":
 
         form = LoginForm(data=request.POST)
-        #추가
+        # 추가
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
 
             user = authenticate(username=username, password=password)
-            if user :
+            if user:
                 login(request, user)
-                #수정
+                # 수정
                 # 로그인 시도 횟수 초기화
                 user.login_attempts = 0
                 user.save()
@@ -46,23 +47,23 @@ def login_view(request):
                 # print("로그인에 실패했습니다.")
                 form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다.")
 
-
         context = {
-            "form": form ,
+            "form": form,
         }
         return render(request, 'users/login.html', context)
     else:
         form = LoginForm()
         context = {
-            "form" : form
+            "form": form
         }
         return render(request, 'users/login.html', context)
 
 
 def logout_view(request):
     logout(request)
-    #수정
+    # 수정
     return redirect("users:login")
+
 
 def signup(request):
     # 추가
@@ -81,12 +82,10 @@ def signup(request):
     return render(request, 'users/signup.html', context)
 
 
-
-
 def profile(request, user_id):
     user = get_object_or_404(User, id=user_id)
     login_user = request.user
-    posts = Post.objects.filter(user = user).order_by('-created')
+    posts = Post.objects.filter(user=user).order_by('-created')
     # print("user", user)
     # print("login_user", login_user)
     # print("request.user.is_authenticated",request.user.is_authenticated)
@@ -116,13 +115,14 @@ def profile(request, user_id):
 
     context = {
         "user": user,
-        "login_user":login_user,
+        "login_user": login_user,
         "page_obj": page_obj,
         "paginator": paginator,
         # 추가
         "custom_range": custom_range
     }
-    return render(request, 'users/profile.html',context)
+    return render(request, 'users/profile.html', context)
+
 
 def user_edit(request):
     # 추가
@@ -138,7 +138,9 @@ def user_edit(request):
 
     context = {"form": form,
                }
-    return render(request, 'users/profile.html',context)
+    return render(request, 'users/profile.html', context)
+
+
 def profile_edit(request, user_id):
     form = CustomUserChangeForm(instance=request.user)
     user = get_object_or_404(User, id=user_id)
@@ -148,9 +150,10 @@ def profile_edit(request, user_id):
         "user": user,
         "relationships": relationships,
         "login_user": login_user,
-        "form" :form
+        "form": form
     }
-    return render(request, 'users/profile_edit.html',context)
+    return render(request, 'users/profile_edit.html', context)
+
 
 def update_password(request, user_id):
     if request.method == "GET":
@@ -170,15 +173,15 @@ def update_password(request, user_id):
         "user": user,
         "relationships": relationships,
         "login_user": login_user,
-        "form" :form
+        "form": form
     }
-    return render(request, 'users/profile_edit_password.html',context)
+    return render(request, 'users/profile_edit_password.html', context)
 
-def delete_user(request,user_id):
-     request.user.delete()
-     logout(request)
-     return redirect("users:login")
 
+def delete_user(request, user_id):
+    request.user.delete()
+    logout(request)
+    return redirect("users:login")
 
 
 def followers(request, user_id):
@@ -186,8 +189,8 @@ def followers(request, user_id):
     relationships = user.follower_relationships.all()
     login_user = request.user
     context = {
-        "user" : user,
-        "relationships" : relationships,
+        "user": user,
+        "relationships": relationships,
         "login_user": login_user,
     }
     return render(request, 'users/followers.html', context)
@@ -198,8 +201,8 @@ def following(request, user_id):
     relationships = user.following_relationships.all()
     login_user = request.user
     context = {
-        "user" : user,
-        "relationships" : relationships,
+        "user": user,
+        "relationships": relationships,
         "login_user": login_user,
     }
     return render(request, 'users/following.html', context)
@@ -215,6 +218,7 @@ def follow(request, user_id):
         user.following.add(target_user)
     url_next = request.GET.get('next') or reverse('users:profile', args=[user.id])
     return HttpResponseRedirect(url_next)
+
 
 def reset_password(request, user_id):
     if request.method == "GET":
@@ -238,12 +242,9 @@ def reset_password(request, user_id):
     }
     return render(request, 'users/profile_edit_reset_password.html', context)
 
-def send_email(request):
-    subject = "message"
-    to = ["lsy3709@naver.com"]
-    from_email = "lsy3709@gmail.com"
-    message = "메지시 테스트, https://sylovestp.com"
-    EmailMessage(subject=subject, body=message, to=to, from_email=from_email).send()
+
+
+
 
 def forgot_id(request):
     if request.method == "GET":
@@ -256,11 +257,33 @@ def forgot_id(request):
         #     form.save()
         #     return redirect("posts:feeds")
 
-
     context = {
         "form": form
     }
     return render(request, 'users/forgot_id.html', context)
+
+
+
+def ForgotIDView(request):
+    context = {}
+    if request.method == 'POST':
+        email = request.POST.get('find_email')
+        try:
+            user = User.objects.get(email=email)
+            if user is not None:
+                method_email = EmailMessage(
+                    'Your ID is in the email',
+                    str(user.username),
+                    # settings.EMAIL_HOST_USER,
+                    [email],
+                )
+                method_email.send(fail_silently=False)
+                return render(request, 'accounts/id_sent.html', context)
+        except:
+            messages.info(request, "There is no username along with the email")
+    context = {}
+    return render(request, 'users/forgot_id.html', context)
+
 
 def verify_code(request):
     # 추가
@@ -281,9 +304,23 @@ def verify_code(request):
 
     return render(request, 'users/verify_code.html')
 
+
 def auth_email(request):
     return render(request, 'users/auth_email.html')
 
+# def send_email(request):
+#     subject = "message"
+#     to = ["lsy3709@naver.com"]
+#     from_email = "lsy3709@gmail.com"
+#     message = "메지시 테스트, https://sylovestp.com"
+#     EmailMessage(subject=subject, body=message, to=to, from_email=from_email).send()
+def send_email(subject, to,message):
+    subject_send = subject
+    to_send = []
+    to_send.append(to)
+    from_email = "lsy3709@gmail.com"
+    message_send = message
+    EmailMessage(subject=subject_send, body=message_send, to=to_send, from_email=from_email).send()
 
 def send_email_with_code(request):
     if request.method == 'POST':
@@ -292,18 +329,7 @@ def send_email_with_code(request):
         code = ''.join([str(random.randint(0, 9)) for _ in range(6)])
         request.session['verification_code'] = code
         request.session.set_expiry(300)  # 5분 (300초) 후 세션 만료
-        subject = "인증코드"
-        to = []
-        to.append(email)
-        from_email = "lsy3709@gmail.com"
         message = f"인증코드: [{code}]"
-        print(f"code : {code}, email : {email}, subject : {subject}, to : {to}, from : {from_email}, message : {message}")
-        email_message = EmailMessage(
-        subject=subject,
-        body=message,
-        to=to,
-        from_email=from_email
-          )
-        email_message.send()
+        send_email("인증코드", email, message)
         # 성공 시
         return redirect("users:verify_code")  # 인증 코드 확인 페이지로 이동
