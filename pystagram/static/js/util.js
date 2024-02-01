b956fe2f6d7167aa15f49cd67fef7a0470044138
lsy3709 <lsy3709@naver.com>
2024-02-01 17:03:45 +0900

    댓글 달기 아이콘 클릭시, 각 피드글마다 댓글폼 반응 확인 완료

--- pystagram/static/js/util.js
+++ pystagram/static/js/util.js
@@ -9,12 +9,66 @@ $(document).ready(function () {
     });
 
     //스크롤 버튼 부드럽게 동작하기.
+    $(".comment_form_btn").click(function () {
+        // data-post-id를 통해 post.id 값을 가져오기
+        const postId = $(this).data("post-id");
+
+        // 가져온 post.id를 사용하여 원하는 동작 수행
+        toggleCommentForm(postId)
+    });
+
+
     $('#scroll-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 500);
         return false;
     });
 
+function toggleCommentForm(postId) {
+    const showFormBtn = document.getElementById(`comment_form_btn_${postId}`);
+    const commentForm = document.getElementById(`comment_form_${postId}`);
+
+    showFormBtn.addEventListener('click', function () {
+        const computedStyle = getComputedStyle(commentForm);
+
+        if (computedStyle.display === 'none') {
+            commentForm.style.display = 'flex';
+        } else {
+            commentForm.style.display = 'none';
+        }
+    });
+}
+
+    // script.js
+
+
+    const openModalBtn = document.getElementById('openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const modal = document.getElementById('myModal');
+    const messageForm = document.getElementById('messageForm');
+
+    openModalBtn.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    closeModalBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    messageForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const messageTitle = document.getElementById('messageTitle').value;
+        const messageContent = document.getElementById('messageContent').value;
+
+        // 여기에서 쪽지를 보내는 동작을 수행합니다.
+        // 예: 서버로 데이터 전송 또는 다른 원하는 동작 수행
+
+        alert(`제목: ${messageTitle}\n내용: ${messageContent}`);
+        modal.style.display = 'none';
+    });
+
 
 })
+
+
 
 
