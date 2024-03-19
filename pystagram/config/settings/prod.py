9fe25ebe111f084e8f5835df1af5c5bf0804a5e8
lsy3709 <lsy3709@naver.com>
2024-03-20 07:40:20 +0900

    aws storage bucket 설정27, .env, 파일 불러와짐, 26에서 설정 실행은 되지만, 버킷에 저장은 안됨

--- pystagram/config/settings/prod.py
+++ pystagram/config/settings/prod.py
@@ -11,25 +11,31 @@ env = environ.Env(
 )
 environ.Env.read_env(BASE_DIR / '.env')
 
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
-# AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
-# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-#
-# if AWS_S3_ACCESS_KEY_ID and AWS_S3_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
-#     # 장고 4.2부터 스토리지 클래스 지정방법이 변경되었습니다.
-#     if django.VERSION < (4, 2):
-#         DEFAULT_FILE_STORAGE = "core.storages.aws.AwsMediaStorage"
-#         STATICFILES_STORAGE = "core.storages.aws.AwsStaticStorage"
-#     else:
-#         STORAGES = {
-#             "default": {
-#                 "BACKEND": "core.storages.aws.AwsMediaStorage",
-#             },
-#             "staticfiles": {
-#                 "BACKEND": "core.storages.aws.AwsStaticStorage",
-#             },
-#         }
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+
+AWS_S3_ENDPOINT_URL = 'https://your_lightsail_endpoint'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'the_path_within_your_bucket'
+
+if AWS_S3_ACCESS_KEY_ID and AWS_S3_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
+    # 장고 4.2부터 스토리지 클래스 지정방법이 변경되었습니다.
+    if django.VERSION < (4, 2):
+        DEFAULT_FILE_STORAGE = "core.storages.aws.AwsMediaStorage"
+        STATICFILES_STORAGE = "core.storages.aws.AwsStaticStorage"
+    else:
+        STORAGES = {
+            "default": {
+                "BACKEND": "core.storages.aws.AwsMediaStorage",
+            },
+            "staticfiles": {
+                "BACKEND": "core.storages.aws.AwsStaticStorage",
+            },
+        }
 
 
 ALLOWED_HOSTS = ['52.78.54.206','www.goldmagnetsoft.com','goldmagnetsoft.com']
