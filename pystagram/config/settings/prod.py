817d3681471228bf42e703d59aa71ad96df8432b
lsy3709 <lsy3709@naver.com>
2024-03-20 07:24:52 +0900

    aws storage bucket 설정25, .env, 파일 불러와짐

--- pystagram/config/settings/prod.py
+++ pystagram/config/settings/prod.py
@@ -11,26 +11,6 @@ env = environ.Env(
 )
 environ.Env.read_env(BASE_DIR / '.env')
 
-
-
-ALLOWED_HOSTS = ['52.78.54.206','www.goldmagnetsoft.com','goldmagnetsoft.com']
-# STATIC_ROOT = BASE_DIR / 'static/'
-# STATICFILES_DIRS = []
-DEBUG = False
-
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
-    }
-}
-# get_secret("SECRET_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY')
@@ -50,3 +30,23 @@ if AWS_S3_ACCESS_KEY_ID and AWS_S3_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME
                 "BACKEND": "core.storages.aws.AwsStaticStorage",
             },
         }
+
+
+ALLOWED_HOSTS = ['52.78.54.206','www.goldmagnetsoft.com','goldmagnetsoft.com']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
+# get_secret("SECRET_KEY")
