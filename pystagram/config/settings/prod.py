838611df5edec1aba9eab73687aa752a76e983e8
lsy3709 <lsy3709@naver.com>
2024-03-20 05:44:39 +0900

    aws storage bucket 설정12, .env

--- pystagram/config/settings/prod.py
+++ pystagram/config/settings/prod.py
@@ -15,7 +15,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 ALLOWED_HOSTS = ['52.78.54.206','www.goldmagnetsoft.com','goldmagnetsoft.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-# DEBUG = False
+DEBUG = False
 
 
 
@@ -29,13 +29,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-#
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
-AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY')
-AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+# get_secret("SECRET_KEY")
+AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ACCESS_KEY_ID = get_secret('AWS_S3_ACCESS_KEY_ID')
+AWS_S3_SECRET_ACCESS_KEY = get_secret('AWS_S3_SECRET_ACCESS_KEY')
+AWS_S3_REGION_NAME = get_secret('AWS_S3_REGION_NAME')
 
-if env('AWS_STORAGE_BUCKET_NAME') and env('AWS_S3_SECRET_ACCESS_KEY') and env('AWS_STORAGE_BUCKET_NAME'):
+if AWS_S3_ACCESS_KEY_ID and AWS_S3_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
     # 장고 4.2부터 스토리지 클래스 지정방법이 변경되었습니다.
     if django.VERSION < (4, 2):
         DEFAULT_FILE_STORAGE = "core.storages.aws.AwsMediaStorage"
