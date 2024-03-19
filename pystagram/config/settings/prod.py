b1da3ba3461540b0a89972deef209d9f55cfca08
lsy3709 <lsy3709@naver.com>
2024-03-19 16:58:38 +0900

    aws storage bucket 설정, .env

--- pystagram/config/settings/prod.py
+++ pystagram/config/settings/prod.py
@@ -2,6 +2,23 @@ import environ
 
 from .base import *
 
+import django
+
+if AWS_S3_ACCESS_KEY_ID and AWS_S3_SECRET_ACCESS_KEY and AWS_STORAGE_BUCKET_NAME:
+    # 장고 4.2부터 스토리지 클래스 지정방법이 변경되었습니다.
+    if django.VERSION < (4, 2):
+        DEFAULT_FILE_STORAGE = "core.storages.aws.AwsMediaStorage"
+        STATICFILES_STORAGE = "core.storages.aws.AwsStaticStorage"
+    else:
+        STORAGES = {
+            "default": {
+                "BACKEND": "core.storages.aws.AwsMediaStorage",
+            },
+            "staticfiles": {
+                "BACKEND": "core.storages.aws.AwsStaticStorage",
+            },
+        }
+
 ALLOWED_HOSTS = ['52.78.54.206','www.goldmagnetsoft.com','goldmagnetsoft.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
