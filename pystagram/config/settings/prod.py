c09ebd42d2a72d7a6a62d28121465915cc95d1bb
lsy3709 <lsy3709@naver.com>
2024-03-20 09:49:08 +0900

    aws bucket setting 확인 2

--- pystagram/config/settings/prod.py
+++ pystagram/config/settings/prod.py
@@ -18,9 +18,9 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 # 이 설정으로 반응은 보임
 # AWS_S3_ENDPOINT_URL = 'https://bucket-4whmj9.s3.ap-northeast-2.amazonaws.com/static2'
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 # AWS_LOCATION = 'static2'
 # 이 설정으로 반응은 보임
 
